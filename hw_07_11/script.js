11e3bc412305a717224e72f2884d6dbe861d3fed
tati1129 <tatka.bogat@gmail.com>
2022-11-10 00:06:36 +0100

    change hw 07.11

--- hw_07_11/script.js
+++ hw_07_11/script.js
@@ -1,14 +1,25 @@
-
+// 1. Создайте кнопку в html при нажатию на которую в консоль выводится "привет".
 
 let showHideBtn = document.querySelector('.button-show-hide');
 let test = document.querySelector('.test');
 showHideBtn.addEventListener('click', showHideBlock);
-test.addEventListener('click',showHideBlock)
+test.addEventListener('click',showHideBlock);
+
 
 function showHideBlock(event) {
     test.classList.toggle('hiden-block');
     test.classList.toggle('show-block');
     showHideBtn.classList.toggle('show-btn');
     showHideBtn.classList.toggle('hide-btn');
-    console.log('Привет!');
+    console.log(test.textContent);
+}
+
+// 2. Создайте блок в html c картинкой и кнопку. При нажатии на кнопку блок с картинкой должен получать border-radius 50px и overflow hidden. При нажатии второй раз на кнопку стили должны убираться и возвращаться к исходным. Реализуйте через classList.toggle()
+
+let image = document.querySelector('.image');
+let actionButton = document.querySelector('.button-action');
+actionButton.addEventListener('click', getStyle);
+
+function getStyle(event) {
+    image.classList.toggle('get-style');
 }
